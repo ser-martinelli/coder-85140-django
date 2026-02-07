3ec7f86d480979aaf5d1d6843bad839a8f320ee8
Sergio Martinelli <ser.r.martinelli@gmail.com>
2026-02-06 21:08:09 -0300

    Subiendo más cambios

--- src/prueba/views.py
+++ src/prueba/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    datos = {
+        "titulo": "Django",
+        "descripcion": "Framework para crear aplicaciones web",
+    }
+    return render(request, "prueba/index.html", datos)
