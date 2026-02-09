b3c8c488a0d0e330fd4fb21752a38ef639c736a9
Sergio Martinelli <ser.r.martinelli@gmail.com>
2026-02-08 21:18:10 -0300

    Pruebas con plantillas

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from prueba.views import index
+from prueba.views import index, notas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("prueba/", index),
+    path("notas/", notas),
 ]
--- src/prueba/views.py
+++ src/prueba/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "prueba/index.html", {"titulo": "Python🔥 "})
+    datos = {
+        "titulo": "Django",
+        "descripcion": "Framework para crear aplicaciones web",
+    }
+    return render(request, "prueba/index.html", datos)
+
+def notas(request):
+    mis_notas = [10, 7, 6, 3, 8, 4]
+    return render(request, "prueba/notas.html", {"notas": mis_notas})
